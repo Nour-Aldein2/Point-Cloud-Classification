c8d82c1c64e01f2280d9bcc91a5364fb3704fed0
Nour-Aldein2 <nour.aldein002.2@gmail.com>
2026-08-09 18:42:27 +0900

    prepared training loop for baseline

--- Baseline/config.py
+++ Baseline/config.py
@@ -12,13 +12,41 @@ class DataConfig:
     noise_sigma: float = 0.02
     seed: int = 11
 
+    batch_size: int = 64
+    num_workers: int = 4
+    pin_memory: bool = True
+
+
+@dataclass
+class Model:
+    num_classes: int = 10
+
+    hidden_dim1: int = 64
+    hidden_dim2: int = 128
+    hidden_dim3: int = 256
+
+    kernel_size: int = 1
+
+    dropout_rate: float = 0.3
+
+class Training:
+    epochs: int = 1000
+    es_patience: int = 50
+
+    learning_rate: float = 1e-3
+
+    save_path: str | Path = Path("./checkpoints")
+
 
 @dataclass
 class Config:
     data: DataConfig = field(default_factory=DataConfig)
+    model: Model = field(default_factory=Model)
+    training: Training = field(default_factory=Training)
 
 
 # TODO: How to augment 3D images?
 # TODO: Consider merging train and test and resampling such that each class is 80:5:15
 ## TODO: Rotation should be done along different axes not only z!
 ## TODO: Consider reflection!
+## TODO: Develop voxelisation
--- Baseline/train.py
+++ Baseline/train.py
@@ -1,13 +1,147 @@
-train_dataset = PointCloudData(
-    root_dir=cfg.data.path,
-    num_points=2048,
-    split_name="train",
-    seed=42,
-)
+from pathlib import Path
 
-test_dataset = PointCloudData(
-    root_dir=cfg.data.path,
-    num_points=2048,
-    split_name="test",
-    seed=42,
-)
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+
+from tqdm import tqdm
+
+from config import Config
+from data_utils import PointCloudData
+from model import PointNetBaseline
+
+
+def one_epoch(model, optimizer, loss_fcn, train_loader, val_loader, device):
+    train_loss = 0.0
+    train_correct = 0
+    train_samples = 0
+    model.train()
+    for batch in tqdm(train_loader, desc="Training"):
+        points = batch["point_cloud"].to(device)
+        labels = batch["category"].to(device)
+
+        optimizer.zero_grad(set_to_none=True)
+
+        logits = model(points)
+        loss = loss_fcn(logits, labels)
+
+        loss.backward()
+        optimizer.step()
+
+        batch_size = labels.size(0)
+
+        train_loss += loss.item() * batch_size
+        train_correct += (logits.argmax(dim=1) == labels).sum().item()
+
+        train_samples += batch_size
+
+    train_loss /= train_samples
+    train_acc = train_correct / train_samples
+
+    val_loss = 0.0
+    val_correct = 0
+    val_samples = 0
+
+    model.eval()
+    with torch.inference_mode():
+        for batch in tqdm(val_loader, desc="Validating"):
+            points = batch["point_cloud"].to(device)
+            labels = batch["category"].to(device)
+
+            logits = model(points)
+            loss = loss_fcn(logits, labels)
+
+            batch_size = labels.size(0)
+
+            val_loss += loss.item() * batch_size
+            val_correct += (logits.argmax(dim=1) == labels).sum().item()
+
+            val_samples += batch_size
+
+    val_loss /= val_samples
+    val_acc = val_correct / val_samples
+
+    return train_loss, train_acc, val_loss, val_acc
+
+
+
+
+if __name__ == "__main__":
+    cfg = Config()
+    device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
+
+    train_dataset = PointCloudData(root_dir=cfg.data.path,
+                                   num_points=cfg.data.num_points,
+                                   split_name="train",
+                                   seed=cfg.data.seed)
+    test_dataset = PointCloudData(root_dir=cfg.data.path,
+                                  num_points=cfg.data.num_points,
+                                  split_name="test",
+                                  seed=cfg.data.seed)
+
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=cfg.data.batch_size,
+        shuffle=True,
+        num_workers=cfg.data.num_workers,
+        pin_memory=cfg.data.pin_memory,
+    )
+
+    test_loader = DataLoader(
+        test_dataset,
+        batch_size=cfg.data.batch_size,
+        shuffle=False,
+        num_workers=cfg.data.num_workers,
+        pin_memory=cfg.data.pin_memory,
+    )
+
+    baseline_model = PointNetBaseline(cfg).to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(baseline_model.parameters(), lr=cfg.training.learning_rate)
+
+    checkpoint_dir = cfg.training.save_path
+    checkpoint_dir.mkdir(exist_ok=True)
+
+    best_val_loss = float("inf")
+    es_counter = 0
+    for epoch in range(cfg.training.epochs):
+
+        train_loss, train_acc, val_loss, val_acc = one_epoch(
+            model=baseline_model,
+            optimizer=optimizer,
+            loss_fcn=criterion,
+            train_loader=train_loader,
+            val_loader=test_loader,
+            device=device,
+        )
+
+        print(
+            f"Epoch {epoch + 1}/{cfg.training.epochs} | "
+            f"train loss: {train_loss:.4f} | "
+            f"train acc: {train_acc:.2%} | "
+            f"val loss: {val_loss:.4f} | "
+            f"val acc: {val_acc:.2%}"
+        )
+
+        if val_loss < best_val_loss:
+            es_counter = 0
+            print(f"✅ New best model found val_loss={val_loss:.4f} (prev ={best_val_loss:.4f})")
+            best_val_loss = val_loss
+
+            torch.save(
+                {
+                    "epoch": epoch + 1,
+                    "model_state_dict": baseline_model.state_dict(),
+                    "optimizer_state_dict": optimizer.state_dict(),
+                    "val_loss": val_loss,
+                    "val_acc": val_acc,
+                },
+                checkpoint_dir / "best_model.pt",
+            )
+        else:
+            es_counter += 1
+
+        if es_counter >= cfg.training.es_patience:
+            print("Early stopping triggered!")
+            break
